d8a84c5bf504638443803bcb7687cc0c8de73411
Nicolas Bourré <nbourrereg@hotmail.com>
2020-04-13 19:36:17 -0400

    I changed something?

--- wpf_master_detail/Models/Student.cs
+++ wpf_master_detail/Models/Student.cs
@@ -4,6 +4,7 @@ using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Runtime.CompilerServices;
 using System.Text;
+using System.Linq;
 
 namespace wpf_master_detail.Models
 {
@@ -59,6 +60,7 @@ namespace wpf_master_detail.Models
                 OnPropertyChanged();
             }
         }
+            
 
 
         public event PropertyChangedEventHandler PropertyChanged;
